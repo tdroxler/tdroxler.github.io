793aceea80a62526c267d4fe43ff1ff6f9a18666
Thomas Droxler <thomas.droxler@gmail.com>
2025-07-14 16:13:04 +0200

    WIP

--- .github/scripts/generate_songs_index.sh
+++ .github/scripts/generate_songs_index.sh
@@ -1,22 +1,35 @@
 #!/usr/bin/env bash
+#
+# OUTPUT="songs.json"
+#
+# # Find all .cho files, build JSON lines
+# find "public" -type f -name "*.cho" | \
+# while read -r file; do
+#   echo $file
+#   # # Path relative to public/
+#   # # Folder relative to public/songs
+#   # folder=$(dirname "${file#public/songs/}")
+#   # [ "$folder" = "." ] && folder="" # root folder becomes empty
+#   # # Title is filename without extension
+#   # fname=$(basename "$file")
+#   # title="${fname%.cho}"
+#   # # Emit JSON object
+#   # printf '{ "title": "%s", "path": "%s", "folder": "%s" }\n' \
+#   #   "$title" "$file" "$folder"
+# done #| \
+# # Combine into a JSON array using jq
+# #jq -s '.' > "$OUTPUT"
 
-SONGS_DIR="songs"
 OUTPUT="songs.json"
 
 # Find all .cho files, build JSON lines
-find "$SONGS_DIR" -type f -name "*.cho" | \
+find "public" -type f -name "*.cho" | \
 while read -r file; do
-  # Path relative to public/
-  relpath="${file#public/}"
-  # Folder relative to public/songs
-  folder=$(dirname "${file#public/songs/}")
-  [ "$folder" = "." ] && folder="" # root folder becomes empty
-  # Title is filename without extension
-  fname=$(basename "$file")
-  title="${fname%.cho}"
-  # Emit JSON object
-  printf '{ "title": "%s", "path": "%s", "folder": "%s" }\n' \
-    "$title" "$relpath" "$folder"
-done | \
+   fname=$(basename "$file")
+   title="${fname%.cho}"
+  # # Emit JSON object
+   printf '{ "title": "%s", "path": "%s" }\n' \
+     "$title" "$file"
+done  |\
 # Combine into a JSON array using jq
 jq -s '.' > "$OUTPUT"
